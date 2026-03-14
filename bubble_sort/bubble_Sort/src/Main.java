6ffb0722f790a05d0cbec86052bd3ceb6f48b1cc
snoop-exe <tioespertonaoburro@gmail.com>
2026-03-14 11:06:06 -0300

    Primeira Atividade bubble sort

---
+++ bubble_sort/bubble_Sort/src/Main.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+import java.util.Random;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        
+        int[] vetor = new int[1000];
+        Random random = new Random();
+        for (int i = 0; i < vetor.length; i++) {
+            vetor[i] = i;
+        }
+
+        for (int i = 0; i < vetor.length; i++) {
+            int j = random.nextInt(vetor.length);
+
+            int aux = vetor[i];
+            vetor[i] = vetor[j];
+            vetor[j] = aux;
+        }
+        System.out.println("Vetor desordenado: " + Arrays.toString(vetor));
+       int aux;
+       boolean controle;
+       long inicio = System.currentTimeMillis();
+       int cont = 0;
+
+       for(int i = 0; i < vetor.length;i++) {
+           controle = true;
+           System.out.println("Vetor ordenado: " + Arrays.toString(vetor));
+           for (int j = 0; j < (vetor.length - 1); j++) {
+               if (vetor[j] > vetor[j + 1]) {
+                   aux = vetor[j];
+                   vetor[j] = vetor[j + 1];
+                   vetor[j + 1] = aux;
+                   controle = false;
+                   cont++;
+               }
+           }
+           if (controle) {
+               break;
+           }
+
+       }
+        long fim = System.currentTimeMillis();
+        System.out.println("Tempo gasto: " + (fim - inicio) + "ms");
+        System.out.println("Modificações no vetor: " + cont);
+    }
+}
